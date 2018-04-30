a48dc6af332c36f2fc9014f1ac7f92cdbbb43b27
Nayzee <35280493+Nayzee@users.noreply.github.com>
2018-04-29 19:44:24 -0500

    Final Edit V0.1

--- app/src/main/java/com/example/donovan/epicrunningsim/ResultsActivity.java
+++ app/src/main/java/com/example/donovan/epicrunningsim/ResultsActivity.java
@@ -22,12 +22,14 @@ public class ResultsActivity extends AppCompatActivity {
     //create variable for back button
     private Button backButton;
     private Button upgradeButton;
+    private Button runButton;
     private TextView coinsCollected;
     private TextView totalCoins;
     private TextView upgrades;
     private int numOfCoins;
     private int numOfUpgrades;
     int coinsPerRun = 100;
+
 
 
     @Override
@@ -38,12 +40,12 @@ public class ResultsActivity extends AppCompatActivity {
         //attach variables to buttons and textviews in layout
         backButton = (Button) findViewById(R.id.btnBack);
         upgradeButton = (Button) findViewById(R.id.btnUpgrade);
+        runButton = (Button) findViewById(R.id.btnRun);
         totalCoins = (TextView) findViewById(R.id.txtTotalCoins);
         coinsCollected = (TextView) findViewById(R.id.txtCoinsCollected);
         upgrades = (TextView) findViewById(R.id.txtNumberOfUpgrades);
 
-        //display the number of coins
-        coinsCollected.setText(getString(R.string.coins_collected) + " " + coinsPerRun);
+
 
         //create file to store coins and upgrades
         final SharedPreferences sharedPrefCoins = getApplicationContext().getSharedPreferences(
@@ -53,20 +55,31 @@ public class ResultsActivity extends AppCompatActivity {
         final SharedPreferences.Editor editorCoins = sharedPrefCoins.edit();
         final SharedPreferences.Editor editorUpgrades = sharedPrefUpgrades.edit();
 
-        //get the number of coins and upgrades the user currently has
-        numOfCoins = sharedPrefCoins.getInt("numberOfCoins", 0);
-        numOfUpgrades = sharedPrefUpgrades.getInt("numberOfUpgrades", 0);
 
-        //take the coins from this run and add it to the total coins
-        numOfCoins = numOfCoins + coinsPerRun;
 
-        //update the number of coins in the shared preferences file
-        editorCoins.putInt("numberOfCoins", numOfCoins);
-        editorCoins.apply();
+        runButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //display the number of coins collected in a run
+                coinsCollected.setText(getString(R.string.coins_collected) + " " + coinsPerRun);
 
-        //update the display for the coins
-        totalCoins.setText(getString(R.string.total_coins) + " " + sharedPrefCoins.getInt("numberOfCoins", 0));
-        upgrades.setText(getString(R.string.upgrades) + " " + sharedPrefUpgrades.getInt("numberOfUpgrades", 0));
+                //get the number of coins and upgrades the user currently has
+                numOfCoins = sharedPrefCoins.getInt("numberOfCoins", 0);
+                numOfUpgrades = sharedPrefUpgrades.getInt("numberOfUpgrades", 0);
+
+                //take the coins from this run and add it to the total coins
+                numOfCoins = numOfCoins + coinsPerRun;
+
+                //update the number of coins in the shared preferences file
+                editorCoins.putInt("numberOfCoins", numOfCoins);
+                editorCoins.apply();
+
+                //update the display for the coins
+                totalCoins.setText(getString(R.string.total_coins) + " " + sharedPrefCoins.getInt("numberOfCoins", 0));
+                upgrades.setText(getString(R.string.upgrades) + " " + sharedPrefUpgrades.getInt("numberOfUpgrades", 0));
+
+            }
+        });
 
         //buy an upgrade
         upgradeButton.setOnClickListener(new View.OnClickListener() {
