8c1a2164150bf8cd9446777d09370611769c02dc
Nayzee <35280493+Nayzee@users.noreply.github.com>
2018-05-01 07:16:54 -0500

    Final Edit v1.0

--- app/src/main/java/com/example/donovan/epicrunningsim/ResultsActivity.java
+++ app/src/main/java/com/example/donovan/epicrunningsim/ResultsActivity.java
@@ -84,7 +84,9 @@ public class ResultsActivity extends AppCompatActivity {
                     upgrades.setText(getString(R.string.upgrades) + " " + sharedPrefUpgrades.getInt("numberOfUpgrades", 0));
                 }
                 else {
-                    
+                    //let the user know if they have too many coins
+                    Toast toast = Toast.makeText(getApplicationContext(), getString(R.string.too_many_coins), Toast.LENGTH_SHORT);
+                    toast.show();
                 }
 
 
@@ -98,30 +100,38 @@ public class ResultsActivity extends AppCompatActivity {
         upgradeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (numOfCoins >= 100) {
-                    //subtract coins to purchase an upgrade
-                    numOfCoins -= 100;
-                    numOfUpgrades += 1;
-                    upgradeMultiplier = (numOfUpgrades * 0.5) + 1;
-                    coinsPerRun = (int) Math.round(50 * upgradeMultiplier);
-                    coinsCollected.setText(getString(R.string.coins_collected) + " " + coinsPerRun);
+                if (numOfUpgrades <= 2000000000){
+                    if (numOfCoins >= 100) {
+                        //subtract coins to purchase an upgrade
+                        numOfCoins -= 100;
+                        numOfUpgrades += 1;
+                        upgradeMultiplier = (numOfUpgrades * 0.5) + 1;
+                        coinsPerRun = (int) Math.round(50 * upgradeMultiplier);
+                        coinsCollected.setText(getString(R.string.coins_collected) + " " + coinsPerRun);
 
 
-                    //update the number of coins and the number of upgrades
-                    editorUpgrades.putInt("numberOfUpgrades", numOfUpgrades);
-                    editorUpgrades.apply();
-                    editorCoins.putInt("numberOfCoins", numOfCoins);
-                    editorCoins.apply();
+                        //update the number of coins and the number of upgrades
+                        editorUpgrades.putInt("numberOfUpgrades", numOfUpgrades);
+                        editorUpgrades.apply();
+                        editorCoins.putInt("numberOfCoins", numOfCoins);
+                        editorCoins.apply();
 
-                    //update the display for coins and upgrades
-                    upgrades.setText(getString(R.string.upgrades) + " " + sharedPrefUpgrades.getInt("numberOfUpgrades", 0));
-                    totalCoins.setText(getString(R.string.total_coins) + " " + sharedPrefCoins.getInt("numberOfCoins", 0));
+                        //update the display for coins and upgrades
+                        upgrades.setText(getString(R.string.upgrades) + " " + sharedPrefUpgrades.getInt("numberOfUpgrades", 0));
+                        totalCoins.setText(getString(R.string.total_coins) + " " + sharedPrefCoins.getInt("numberOfCoins", 0));
+                    }
+                    else{
+                        //let the user know if they do not have enough coins to buy an upgrade
+                        Toast toast = Toast.makeText(getApplicationContext(), getString(R.string.no_coins_message), Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
                 }
-                else{
-                    //let the user know if they do not have enough coins to buy an upgrade
-                    Toast toast = Toast.makeText(getApplicationContext(), getString(R.string.no_coins_message), Toast.LENGTH_SHORT);
+                else {
+                    //let the user know if they have too many upgrades
+                    Toast toast = Toast.makeText(getApplicationContext(), getString(R.string.too_many_upgrades), Toast.LENGTH_SHORT);
                     toast.show();
                 }
+
 
             }
         });
